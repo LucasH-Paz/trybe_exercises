02c6548a0a35971daf997405c8ca307fe02bb59a
lucas-paz <lucas.hpaz@outlook.com>
2021-03-03 16:12:39 -0300

    Adiciona algumas praticas da parte2 de DOM

---
+++ bloco5/dia1/scripts.js
@@ -0,0 +1,8 @@
+console.log(document.getElementById("page-title").innerText = 'The empire strikes back!');
+document.getElementById("second-paragraph").innerText = 'The best star wars episode ever! (Except for the episode III)';
+document.getElementById("subtitle").innerText = 'My favorite SW movies';
+document.getElementById('paragraph'); 
+  paragraph.style.color = "Yellow" ;
+document.getElementsByClassName("movie")[1].style.color = 'blue'
+document.getElementsByTagName("h4")[0].innerText = 'That is it!'
+
